f9fc25373dd65fb32901196a0b272f274c0166a8
yuliiapavlyk <pavlyk.yuliia@gmail.com>
2018-06-17 19:38:49 +0300

    lint project

--- src/app/auth/auth.interceptor.ts
+++ src/app/auth/auth.interceptor.ts
@@ -1,9 +1,9 @@
-import { HttpInterceptor, HttpRequest, HttpHandler, HttpUserEvent, HttpEvent } from "@angular/common/http";
-import { Observable } from "rxjs";
-import { UserService } from "../common/services/user.service";
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpUserEvent, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserService } from '../common/services/user.service';
 import { tap } from 'rxjs/operators';
-import { Injectable } from "@angular/core";
-import { Router } from "@angular/router";
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -11,24 +11,24 @@ export class AuthInterceptor implements HttpInterceptor {
     constructor(private router: Router) { }
 
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if (req.headers.get('No-Auth') == "True")
+        if (req.headers.get('No-Auth') === 'true') {
             return next.handle(req.clone());
+        }
 
         if (localStorage.getItem('userToken') != null) {
             const clonedreq = req.clone({
-                headers: req.headers.set("Authorization", "Bearer " + localStorage.getItem('userToken'))
+                headers: req.headers.set('Authorization', 'Bearer ' + localStorage.getItem('userToken'))
             });
             return next.handle(clonedreq)
                 .pipe(tap(
-                succ => { },
-                err => {
-                    if (err.status === 401)
-                        this.router.navigateByUrl('/main-page');
-                }
+                    succ => { },
+                    err => {
+                        if (err.status === 401) {
+                            this.router.navigateByUrl('/main-page');
+                        }
+                    }
                 ));
         }
-        else {
-            this.router.navigateByUrl('/signin');
-        }
+        this.router.navigateByUrl('/signin');
     }
 }
