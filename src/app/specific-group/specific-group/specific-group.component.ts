d7808602b95ca6b6a4cae60c0c107b0fafad3edf
Roman <maksymyshyn.roman@gmail.com>
2018-06-22 03:49:30 +0300

    group service created

--- src/app/specific-group/specific-group/specific-group.component.ts
+++ src/app/specific-group/specific-group/specific-group.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { Router } from "@angular/router";
 
 import { Group } from '../../common/models/group';
 import { User } from '../../common/models/user';
 import { UserService } from '../../common/services/user.service';
 
-import { MatTabsModule } from '@angular/material/tabs';
 import { MatDialog } from '@angular/material';
+import { GroupService } from '../../common/services/group.service';
 
 @Component({
   selector: 'app-specific-group',
@@ -18,25 +19,25 @@ import { MatDialog } from '@angular/material';
 export class SpecificGroupComponent implements OnInit {
   group: Group;
   mentor: User;
-  id: number;
+  linkId: number;
   //todo should be private, but does not work
   /*private*/ subscription: Subscription;
 
   constructor(private userService: UserService,
+    private groupService: GroupService,
     public dialog: MatDialog,
+    private router: Router,
     private activateRoute: ActivatedRoute) {
-    this.subscription = activateRoute.params.subscribe(params => this.id = params['id']);
+    this.subscription = activateRoute.params.subscribe(params => this.linkId = params['id']);
   }
 
   ngOnInit() {
-    this.group = {
-      Id: this.id,
-      Name: 'LV319.NET',
-      MentorId: 1
+    if (this.linkId != null) {
+      this.groupService.getGroup(this.linkId).subscribe((data: Group) => this.group = data);
+      this.userService.getUser(this.linkId).subscribe((data: User) => this.mentor = data);
+    } else {
+      this.router.navigate(['/main-page']);
     };
-    this.userService.getUser(this.group.MentorId).subscribe((data: User) => {
-      this.mentor = data;
-    });
   }
 
   /* openAddDialog(): void {
