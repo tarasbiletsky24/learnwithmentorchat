ac00d776760b7a90d94f71cc71149e85f51cb68e
yuliiapavlyk <pavlyk.yuliia@gmail.com>
2018-07-08 20:29:50 +0300

    added changing icons for plan

--- src/app/add-tasks/add-tasks.component.ts
+++ src/app/add-tasks/add-tasks.component.ts
@@ -1,10 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { TaskService } from '../common/services/task.service';
 import { Router } from '@angular/router';
 import { Task } from '../../../src/app/common/models/task';
-import { MatDialogRef } from '@angular/material';
+
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 import { MatPaginator, MatTableDataSource, MatRadioButton } from '@angular/material';
+import { Observable } from 'rxjs';
+import { CreatePlanComponent } from '../create-plan/create-plan.component';
+
 
 @Component({
   selector: 'app-add-tasks',
@@ -14,19 +18,29 @@ import { MatPaginator, MatTableDataSource, MatRadioButton } from '@angular/mater
 export class AddTasksComponent implements OnInit {
 
   tasks: Task[];
+  idTaskForAdd: number;
+  idTasks: number[];
+
 
   dataSource = new MatTableDataSource<Task>(this.tasks);
   displayedColumns = ['Name', 'Description', 'Add'];
-  constructor(private taskService: TaskService,
-    private router: Router) { }
-  getPlan(id: number) {
-    if (id == null) {
-      return false;
-    }
-    console.log(id);
-
-
+  constructor(private taskService: TaskService, public thisDialogRef: MatDialogRef<AddTasksComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: CreatePlanComponent
+  ) {
+    this.idTasks = new Array;
   }
+  getTask(event: any,id: number) {
+    this.idTasks.push(id);
+    event.currentTarget.setAttribute('disabled', 'disabled');
+  }
+  console(){
+    this.idTasks.forEach(element => {
+      console.log(element);
+    });
+   
+    
+  }
+  
   ngOnInit() {
     this.taskService.getTasks().subscribe(
       task => this.tasks = task
--- src/app/create-plan/create-plan.component.ts
+++ src/app/create-plan/create-plan.component.ts
@@ -1,19 +1,77 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { AddTasksComponent } from '../add-tasks/add-tasks.component';
+import { Observable } from 'rxjs';
+import { Task } from '../../../src/app/common/models/task';
+import { createElementCssSelector } from '@angular/compiler';
+import { Image } from '.././common/models/image';
+
+import { AlertWindowsComponent } from '.././components/alert-windows/alert-windows.component';
+
+import { DomSanitizer } from '@angular/platform-browser';
+
+import { PlanService } from '.././common/services/plan.service';
+
+export interface AddTasksComponent{
+  idTasks;
+}
 
 @Component({
   selector: 'app-create-plan',
   templateUrl: './create-plan.component.html',
   styleUrls: ['./create-plan.component.css']
 })
+
+
 export class CreatePlanComponent implements OnInit {
-
-  constructor(private dialog: MatDialog) { }
-  openAddTasks() {
-    const dialogRef = this.dialog.open(AddTasksComponent, {});
+  tasks: Observable<Task>;
+  idTasks: number[];
+  selectedFile: File = null;
+  private maxImageSize: number = 1024 * 1024;
+  imageData = '../../../assets/images/1.png';
+  selectedImage = null;
+  constructor(private dialog: MatDialog,
+    private planService: PlanService,
+    private alertWindow: AlertWindowsComponent) {
+      
   }
+
+  openAddTasks(): void {
+    const dialogRef = this.dialog.open(AddTasksComponent, {
+      width: '1000px',
+      data: { idTasks:this.idTasks}        
+
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log("!!!!");
+      console.log(result);
+      
+      this.idTasks=result;
+    
+
+    });
+  }
+  createPlan() {
+    console.log(this.idTasks);
+    
+  }
+  onFileSelected(event) {
+    this.selectedFile = event.target.files[0];
+    if (this.selectedFile.size > this.maxImageSize) {
+      this.alertWindow.openSnackBar(`Image size must be less then ${this.maxImageSize / (1024 * 1024)} mb, please select another`, 'Ok');
+      this.selectedFile = null;
+    } else {
+      const preview = document.getElementById('newImage') as HTMLImageElement;
+      const reader = new FileReader();
+      reader.onloadend = function () {
+        preview.src = reader.result;
+      };
+      reader.readAsDataURL(this.selectedFile);
+    }
+  }
+
+
   ngOnInit() {
-  }
 
+}
 }
