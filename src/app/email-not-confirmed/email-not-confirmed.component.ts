c9b658c3c206367b75198da1b90c10765414697e
Roman Maksymyshyn <maksymyshyn.roman@gmail.com>
2018-08-06 17:05:06 +0300

    added Warning page if email not confirmed

--- src/app/email-not-confirmed/email-not-confirmed.component.ts
+++ src/app/email-not-confirmed/email-not-confirmed.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { EmailService } from '../common/services/email.service';
+import { HttpStatusCodeService } from '../common/services/http-status-code.service';
+import { AlertWindowsComponent } from '../components/alert-windows/alert-windows.component';
+import { Email } from '../common/models/email';
+import { AuthService } from '../common/services/auth.service';
 
 @Component({
   selector: 'app-email-not-confirmed',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailNotConfirmedComponent implements OnInit {
 
-  constructor() { }
+  constructor(private emailService: EmailService,
+    private authService: AuthService,
+    private httpStatusCodeService: HttpStatusCodeService,
+    private alertwindow: AlertWindowsComponent) { }
 
   ngOnInit() {
+  }
+
+  sendEmailConfirmationInstructions() {
+    const email: Email = {
+      Email: this.authService.getUserEmail()
+    }
+    this.emailService.sendEmailConfirmationEmail(email).subscribe(
+      resp => {
+        if (this.httpStatusCodeService.isOk(resp.status)) {
+          this.alertwindow.openSnackBar("Confirmation instructions successfully sent", 'Ok');
+        }
+        if (this.httpStatusCodeService.isNoContent(resp.status)) {
+          this.alertwindow.openSnackBar("User not found", 'Ok');
+        }
+      },
+      error => {
+        this.alertwindow.openSnackBar(error.error.Message, 'Ok');
+      }
+    );
   }
 
 }
