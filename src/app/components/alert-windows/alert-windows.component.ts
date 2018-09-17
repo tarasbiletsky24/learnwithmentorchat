ffd35d7beae93df7f864a454389b4349f372cc2b
Nazar Polevyi <32094354+NazarPolevyi@users.noreply.github.com>
2018-09-17 13:08:28 +0300

    Closes #205 making more time for SnackBar pop-ups (#206)

--- src/app/components/alert-windows/alert-windows.component.ts
+++ src/app/components/alert-windows/alert-windows.component.ts
@@ -16,7 +16,7 @@ export class AlertWindowsComponent  implements OnInit {
   }
   openSnackBar(message: string, action: string) {
     this.snackBar.open(message, action, {
-      duration: 2000,
+      duration: 5000,
     });
   }
 
